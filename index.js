9d407c221350205db34fcf9ce84488ceb49cea3e
Alcee Metevier <almetevier@gmail.com>
2020-04-09 17:38:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase(string))
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase(string) === string) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase(string) === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
